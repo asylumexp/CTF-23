518b15ef52c73ebbc96a6303ed6f4fc79de79d4d
Sam Heinz <sam@samheinz.com>
2023-04-28 13:16:48 +1000

    Standardise red4

--- Objects/Red4.py
+++ Objects/Red4.py
@@ -1,35 +1,35 @@
 from GameFrame import RedBot, Globals
 from enum import Enum
 
+
 class STATE(Enum):
-    WAIT = 1
+    BAIT_PREPARE = 1
     ATTACK = 2
-    PREPARE = 3
-    BAIT = 4
+    BAIT_WAIT = 3
+    BAIT_BAIT = 4
     JAIL = 5
-    HOME = 6
+
 
 class Red4(RedBot):
     def __init__(self, room, x, y):
         RedBot.__init__(self, room, x, y)
-        self.curr_state = STATE.WAIT
+        self.curr_state = STATE.BAIT_PREPARE
         try:
             self.set_image("Images/r4.png", 25, 25)
         except FileNotFoundError:
             print("hello this is me making a error checking for the set image we used images in our testing so we actually knew which bot was which if youre seeing this that means we again forgot to remove the set image for red4 which is awkward gotta say so bye have fun doing the competition.")
 
     def tick(self):
-        if self.curr_state == STATE.WAIT:
-            Globals.red_bots[2].bait_bot_prepare(self, 650, 250, STATE.PREPARE)
+        if self.curr_state == STATE.BAIT_PREPARE:
+            Globals.red_bots[2].bait_bot_prepare(
+                self, 650, 250, STATE.BAIT_WAIT)
         elif self.curr_state == STATE.ATTACK:
-            Globals.red_bots[2].general_bot_attack(self, STATE.WAIT)
-        elif self.curr_state == STATE.PREPARE:
-            Globals.red_bots[2].bait_bot_wait(self, STATE.BAIT)
-        elif self.curr_state == STATE.BAIT:
+            Globals.red_bots[2].general_bot_attack(self, STATE.BAIT_PREPARE)
+        elif self.curr_state == STATE.BAIT_WAIT:
+            Globals.red_bots[2].bait_bot_wait(self, STATE.BAIT_BAIT)
+        elif self.curr_state == STATE.BAIT_BAIT:
             Globals.red_bots[2].bait_bot_bait(self, STATE.JAIL)
         elif self.curr_state == STATE.JAIL:
-            Globals.red_bots[2].general_bot_jailed(self, STATE.HOME)
-        elif self.curr_state == STATE.HOME:
-            Globals.red_bots[2].general_bot_return(self, STATE.WAIT)
+            Globals.red_bots[2].general_bot_jailed(self, STATE.BAIT_PREPARE)
         else:
-            self.curr_state = STATE.WAIT
+            self.curr_state = STATE.BAIT_PREPARE
