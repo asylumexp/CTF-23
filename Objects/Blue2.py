76cbf4eb3ab6534a272a9a89f154e2653cc1583a
Will Coggins <coggiw1@shalomcollege.com>
2023-10-13 08:49:32 +1000

    updated blue bot

--- Objects/Blue2.py
+++ Objects/Blue2.py
@@ -1,4 +1,4 @@
-from GameFrame import RedBot, Globals, BlueBot
+from GameFrame import BlueBot, Globals
 from enum import Enum
 
 
@@ -7,24 +7,32 @@ class STATE(Enum):
     ATTACK = 1
     JAILBREAK = 2
     RETURN = 3
+    TESTING = 4
 
 
 class Blue2(BlueBot):
     def __init__(self, room, x, y):
         BlueBot.__init__(self, room, x, y)
-        self.set_image("Images/b2.png", 25, 25)
-
         self.curr_state = STATE.RETURN
+        try:
+            self.set_image("Images/b2.png", 25, 25)
+        except FileNotFoundError:
+            print("hello this is me making a error checking for the set image we used images in our testing so we actually knew which bot was which if youre seeing this that means we again forgot to remove the set image for blue2 which is awkward gotta say so bye have fun doing the competition.")
+
 
     def tick(self):
-        if self.x > 620:
-            self.curr_state == STATE.RETURN
         # Lame declaring outside init becuz of weird glitch with gameframe
-        self.psuedoflagx = Globals.red_flag.x + 250
+        print(f"corey's dumb Blue bot is in {self.curr_state}")
+        print(Globals.blue_bots[1].x, Globals.blue_bots[1].y)
+        self.psuedoflagx = Globals.blue_flag.x - 250
+        if self.x < 660:
+            self.curr_state == STATE.RETURN
         if self.curr_state == STATE.WAIT:
             self.wait()
         elif self.curr_state == STATE.ATTACK:
             self.attack()
+        elif self.curr_state == STATE.TESTING:
+            self.testing()
         elif self.curr_state == STATE.JAILBREAK:
             self.jailbreak()
         elif self.curr_state == STATE.RETURN:
@@ -32,10 +40,18 @@ class Blue2(BlueBot):
         else:
             self.curr_state = STATE.WAIT
 
+    def testing(self):
+        for i in range(1000):
+            self.turn_towards(Globals.blue_flag.x, Globals.blue_flag.y)
+            self.drive_forward(Globals.FAST)
+
     def wait(self):
-        bot, distance = self.closest_enemy_to_flag()
-        if distance < 200:
-            self.curr_state = STATE.ATTACK
+        bot, distance = self.closest_enemy_to_self(True)
+        if distance < 175:
+            if self.x < 600:
+                self.curr_state = STATE.RETURN
+            else:
+                self.curr_state = STATE.ATTACK
         else:
             bot_jailed = False
             for team_bot in Globals.blue_bots:
@@ -44,11 +60,17 @@ class Blue2(BlueBot):
                     break
             if bot_jailed:
                 self.curr_state = STATE.JAILBREAK
+        self.turn_towards(Globals.blue_flag.x + 200, 300)
+        self.drive_forward(Globals.MEDIUM)
+     
+     
+
+        
 
     def attack(self):
-        bot, distance = self.closest_enemy_to_flag()
+        bot, distance = self.closest_enemy_to_self(True)
         angle = self.angleRelative(bot.x, bot.y)
-        self.turn_towards(bot.x - 20, bot.y, Globals.FAST)
+        self.turn_towards(bot.x + 20, bot.y, Globals.FAST)
         if distance < 200 and angle < 70:
             self.drive_forward(Globals.FAST)
         if distance > 200:
@@ -63,32 +85,26 @@ class Blue2(BlueBot):
         if not bot_jailed:
             self.curr_state = STATE.RETURN
         else:
-            angle = self.angleRelative(50, 75)
-            self.turn_towards(50, 75, Globals.FAST)
+            angle = self.angleRelative(Globals.SCREEN_WIDTH - 75, Globals.SCREEN_HEIGHT - 100)
+            self.turn_towards(Globals.SCREEN_WIDTH - 75, Globals.SCREEN_HEIGHT - 100, Globals.FAST)
             if angle < 120:
                 self.drive_forward(Globals.FAST)
 
     def return_home(self):
-        # if self.x <= self.psuedoflagx-40 or self.x >= self.psuedoflagx-50:
-        # self.turn_towards(self.psuedoflagx-40, Globals.red_flag.y, Globals.FAST)
-        # self.drive_forward(Globals.FAST)
-        if (self.point_to_point_distance(self.x, self.y, self.psuedoflagx, Globals.red_flag.y) > 30):
-            i = self.angleRelative(self.psuedoflagx, Globals.red_flag.y)
-            if i < 0 or i > 40:
-                self.turn_towards(self.psuedoflagx, Globals.red_flag.y, Globals.FAST)
-            else:
-                self.drive_forward(Globals.FAST)
+        if (self.point_to_point_distance(self.x, self.y, self.psuedoflagx, Globals.blue_flag.y) > 40):
+            self.turn_towards(self.psuedoflagx, Globals.blue_flag.y, Globals.FAST)
+            self.drive_forward(Globals.FAST)
         else:
             self.curr_state = STATE.WAIT
 
     def closest_enemy_to_flag(self):
-        closest_bot = Globals.red_bots[0]
+        closest_bot = Globals.blue_bots[0]
         shortest_distance = self.point_to_point_distance(
-            closest_bot.x, closest_bot.y, self.psuedoflagx, Globals.red_flag.y
+            closest_bot.x, closest_bot.y, self.psuedoflagx, Globals.blue_flag.y
         )
-        for curr_bot in Globals.red_bots:
+        for curr_bot in Globals.blue_bots:
             curr_bot_dist = self.point_to_point_distance(
-                curr_bot.x, curr_bot.y, self.psuedoflagx, Globals.red_flag.y
+                curr_bot.x, curr_bot.y, self.psuedoflagx, Globals.blue_flag.y
             )
             if curr_bot_dist < shortest_distance:
                 shortest_distance = curr_bot_dist
@@ -106,3 +122,30 @@ class Blue2(BlueBot):
         if angle < 0:
             angle += 360
         return angle
+
+    def closest_enemy_to_self(self, ignore):
+        # todo - make more efficient
+        closest_bot = Globals.blue_bots[0]
+        closer_bot = Globals.blue_bots[0]
+        shortest_distance = self.point_to_point_distance(
+            closest_bot.x, closest_bot.y, self.x, self.y
+        )
+        for curr_bot in Globals.blue_bots:
+            curr_bot_dist = self.point_to_point_distance(
+                curr_bot.x, curr_bot.y, self.x, self.y
+            )
+            for blue_bot in Globals.blue_bots:
+                # * check enemy distance from self to bot from loop
+                if curr_bot_dist < shortest_distance:
+                    curr_teammate_dist = self.point_to_point_distance(
+                        curr_bot.x, curr_bot.y, self.x, self.y
+                    )
+                    # * check if teammate is closer
+                    if curr_teammate_dist < curr_bot_dist and not ignore:
+                        shortest_distance = curr_bot_dist
+                        closest_bot = curr_bot
+                    elif ignore:
+                        shortest_distance = curr_bot_dist
+                        closest_bot = curr_bot
+
+        return closest_bot, shortest_distance
