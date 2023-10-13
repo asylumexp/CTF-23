075048078b61594892dbdcb9b2c02f0fc122d105
Sam Heinz <54530346+samh06@users.noreply.github.com>
2023-10-13 18:22:59 +1000

    theoretically this should ignore baiting and attack bots

--- Objects/Red4.py
+++ Objects/Red4.py
@@ -13,12 +13,19 @@ class Red4(RedBot):
     def __init__(self, room, x, y):
         RedBot.__init__(self, room, x, y)
         self.curr_state = STATE.WAIT
+        self.attack_forced = (False, None)
         try:
             self.set_image("Images/r4.png", 25, 25)
         except FileNotFoundError:
             print("hello this is me making a error checking for the set image we used images in our testing so we actually knew which bot was which if youre seeing this that means we again forgot to remove the set image for red4 which is awkward gotta say so bye have fun doing the competition.")
 
     def tick(self):
+        for bot in Globals.blue_bots:
+            if bot.has_flag:
+                self.curr_state == STATE.ATTACK
+                Globals.red_bots[0].bot4ready = False
+                self.attack_forced = (True, bot)
+                break
         if self.curr_state == STATE.WAIT:
             Globals.red_bots[2].bait_bot_prepare(self, 650, 250, STATE.PREPARE)
         elif self.curr_state == STATE.ATTACK:
